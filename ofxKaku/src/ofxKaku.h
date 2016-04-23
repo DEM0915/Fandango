971266c27a387950a18c2ad2722b80425f02f425
will <willmercedes@gmail.com>
2016-04-24 02:42:16 +0800

    Hide Cursor
    it’s  cleaner

--- ofxKaku/src/ofxKaku.h
+++ ofxKaku/src/ofxKaku.h
@@ -5,6 +5,7 @@ public:
     // Draw kaku tomomi
     static void draw(float x, float y, float radius) {
         ofPushStyle();
+        ofHideCursor();
         ofSetCircleResolution(100);
         ofSetColor(40, 42, 54);
         ofDrawCircle(x, y, radius);
